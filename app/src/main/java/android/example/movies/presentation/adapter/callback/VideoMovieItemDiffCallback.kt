88995d6d56af97a9aa356db1517c94aeb9dba7eb
Arman <arman226891@gmail.com>
2022-05-16 00:39:02 +0500

    add VideoMovieItemDiffCallback

---
+++ app/src/main/java/android/example/movies/presentation/adapter/callback/VideoMovieItemDiffCallback.kt
@@ -0,0 +1,16 @@
+package android.example.movies.presentation.adapter.callback
+
+import android.example.movies.domain.item.VideoMovieItem
+import androidx.recyclerview.widget.DiffUtil
+
+class VideoMovieItemDiffCallback : DiffUtil.ItemCallback<VideoMovieItem>() {
+
+    override fun areItemsTheSame(oldItem: VideoMovieItem, newItem: VideoMovieItem): Boolean {
+        return oldItem.key == newItem.key
+    }
+
+    override fun areContentsTheSame(oldItem: VideoMovieItem, newItem: VideoMovieItem): Boolean {
+        return oldItem == newItem
+    }
+
+}

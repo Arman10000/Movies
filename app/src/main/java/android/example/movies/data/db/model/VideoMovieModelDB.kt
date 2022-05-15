0a3ccb42ef5cc94aaa68f62254293bffd1eae2ef
Arman <arman226891@gmail.com>
2022-05-16 00:38:47 +0500

    add VideoMovieModelDB

---
+++ app/src/main/java/android/example/movies/data/db/model/VideoMovieModelDB.kt
@@ -0,0 +1,13 @@
+package android.example.movies.data.db.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "video_movie")
+data class VideoMovieModelDB(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val movieId: Int,
+    val key: String,
+    val nameVideo: String
+)

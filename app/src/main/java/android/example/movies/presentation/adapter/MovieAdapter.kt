40350289f8255b20062c1518d53dcde9aad05deb
Arman <arman226891@gmail.com>
2022-05-16 00:42:01 +0500

    add MovieAdapter

---
+++ app/src/main/java/android/example/movies/presentation/adapter/MovieAdapter.kt
@@ -0,0 +1,45 @@
+package android.example.movies.presentation.adapter
+
+import android.example.movies.databinding.MovieItemBinding
+import android.example.movies.domain.item.MovieItem
+import android.example.movies.presentation.adapter.callback.MovieItemDiffCallback
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+class MovieAdapter(
+    private val openDetailMovie: (movieItem: MovieItem) -> Unit
+) : ListAdapter<MovieItem, MovieAdapter.MovieViewHolder>(MovieItemDiffCallback()) {
+
+    class MovieViewHolder(
+        private val binding: MovieItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(
+            movieItem: MovieItem,
+            openDetailMovie: (movieItem: MovieItem) -> Unit
+        ) {
+            Picasso.get().load(movieItem.smallPoster).into(binding.ivSmallPoster)
+            binding.ivSmallPoster.setOnClickListener {
+                openDetailMovie(movieItem)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = MovieItemBinding.inflate(layoutInflater, parent, false)
+        return MovieViewHolder(binding = binding)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bind(
+            movieItem = getItem(position),
+            openDetailMovie = openDetailMovie
+        )
+    }
+
+}

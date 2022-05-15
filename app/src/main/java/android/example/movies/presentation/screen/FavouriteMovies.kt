96f0343b803c0f11c985e9de0f2e25e7194d52e6
Arman <arman226891@gmail.com>
2022-05-16 00:44:41 +0500

    add FavouriteMovies

---
+++ app/src/main/java/android/example/movies/presentation/screen/FavouriteMovies.kt
@@ -0,0 +1,83 @@
+package android.example.movies.presentation.screen
+
+import android.content.Context
+import android.example.movies.R
+import android.example.movies.databinding.FavouriteMoviesBinding
+import android.example.movies.domain.item.MovieItem
+import android.example.movies.presentation.adapter.MovieAdapter
+import android.example.movies.presentation.di.app.App
+import android.example.movies.presentation.viewModel.MovieViewModel
+import android.example.movies.presentation.viewModel.ViewModelFactory
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import javax.inject.Inject
+
+class FavouriteMovies : Fragment(R.layout.favourite_movies) {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+    private val movieViewModel by lazy {
+        activity?.let {
+            ViewModelProvider(
+                it,
+                viewModelFactory
+            )[MovieViewModel::class.java]
+        }
+    }
+    private var _binding: FavouriteMoviesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onAttach(context: Context) {
+        (activity?.application as App).appComponent.inject(this)
+        super.onAttach(context)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu.setGroupVisible(0, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setHasOptionsMenu(true)
+
+        _binding = FavouriteMoviesBinding.bind(view)
+
+        val movieAdapter = MovieAdapter(
+            openDetailMovie = {
+                movieViewModel?.saveDetailsMovie(movieItem = it)
+                findNavController().navigate(
+                    FavouriteMoviesDirections.actionFavouriteMoviesToDetailMovie()
+                )
+            }
+        )
+
+        movieViewModel?.favouriteMovies?.observe(viewLifecycleOwner) { favouriteMovieList ->
+            val movies = arrayListOf<MovieItem>()
+            favouriteMovieList.forEach {
+                movies.add(it.toMovieItem())
+            }
+            movieAdapter.submitList(movies)
+        }
+
+        movieViewModel?.let {
+            binding.rvFavouriteMovies.layoutManager = GridLayoutManager(context, it.getColumnCount())
+        }
+
+        binding.rvFavouriteMovies.adapter = movieAdapter
+
+    }
+
+}
